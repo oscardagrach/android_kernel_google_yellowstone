e5206561e85d82b3e5febda7b55871287082577c
Ryan Grachek <ryan@edited.us>
2019-01-18 10:20:19 -0600

    arm: tegra: yellowstone: remove unused definitions

--- arch/arm/mach-tegra/board-yellowstone.h
+++ arch/arm/mach-tegra/board-yellowstone.h
@@ -20,7 +20,6 @@
 #ifndef _MACH_TEGRA_BOARD_ARDBEG_H
 #define _MACH_TEGRA_BOARD_ARDBEG_H
 
-#include <linux/mfd/as3722-plat.h>
 #include <mach/gpio-tegra.h>
 #include <mach/irqs.h>
 #include "gpio-names.h"
@@ -29,7 +28,6 @@ int ardbeg_emc_init(void);
 int ardbeg_display_init(void);
 int ardbeg_panel_init(void);
 int ardbeg_sdhci_init(void);
-int ardbeg_sata_init(void);
 void arbdeg_sata_clk_gate(void);
 int ardbeg_sensors_init(void);
 int ardbeg_regulator_init(void);
@@ -42,45 +40,12 @@ void shield_new_sysedp_init(void);
 void shield_sysedp_dynamic_capping_init(void);
 void shield_sysedp_batmon_init(void);
 
-
-/* Invensense MPU Definitions */
-#define MPU_GYRO_NAME			"mpu9250"
-#define MPU_GYRO_IRQ_GPIO		TEGRA_GPIO_PR4
-#define MPU_GYRO_ADDR			0x69
-#define MPU_GYRO_BUS_NUM		0
-#define MPU_GYRO_ORIENTATION		MTMAT_TOP_CCW_0
-#define MPU_GYRO_ORIENTATION_E1762	MTMAT_TOP_CCW_270
-#define MPU_COMPASS_NAME		"ak8975"
-#define MPU_COMPASS_ADDR		0x0C
-#define MPU_COMPASS_ADDR_TN8		0x0D
-#define MPU_COMPASS_ORIENTATION		MTMAT_BOT_CCW_270
-#define MPU_BMP_NAME			"bmp280"
-#define MPU_BMP_ADDR			0x77
-
 /* generated soc_therm OC interrupts */
 #define TEGRA_SOC_OC_IRQ_BASE	TEGRA_NR_IRQS
 #define TEGRA_SOC_OC_NUM_IRQ	TEGRA_SOC_OC_IRQ_MAX
 
-/* PCA954x I2C bus expander bus addresses */
-#define PCA954x_I2C_BUS_BASE    6
-#define PCA954x_I2C_BUS0        (PCA954x_I2C_BUS_BASE + 0)
-#define PCA954x_I2C_BUS1        (PCA954x_I2C_BUS_BASE + 1)
-#define PCA954x_I2C_BUS2        (PCA954x_I2C_BUS_BASE + 2)
-#define PCA954x_I2C_BUS3        (PCA954x_I2C_BUS_BASE + 3)
-
-
 #define PALMAS_TEGRA_GPIO_BASE	TEGRA_NR_GPIOS
 #define PALMAS_TEGRA_IRQ_BASE	(TEGRA_SOC_OC_IRQ_BASE + TEGRA_SOC_OC_NUM_IRQ)
-#define AS3722_GPIO_BASE	TEGRA_NR_GPIOS
-#define AS3722_GPIO_END	(AS3722_GPIO_BASE + AS3722_NUM_GPIO)
-
-/* PMU_TCA6416 GPIOs */
-#define PMU_TCA6416_GPIO_BASE   (AS3722_GPIO_END)
-#define PMU_TCA6416_GPIO(x)     (PMU_TCA6416_GPIO_BASE + x)
-#define PMU_TCA6416_NR_GPIOS    18
-/* External peripheral act as interrupt controller */
-/* AS3720 IRQs */
-#define AS3722_IRQ_BASE         (TEGRA_SOC_OC_IRQ_BASE + TEGRA_SOC_OC_NUM_IRQ)
 
 #define CAM_RSTN TEGRA_GPIO_PBB3
 #define CAM1_RSTN TEGRA_GPIO_PCC1
@@ -157,40 +122,6 @@ GPIO, also the GPIO is same for T114 interposer and T124*/
 #define TEGRA_GPIO_I2C4_SDA	TEGRA_GPIO_PV5
 #define TEGRA_GPIO_I2C5_SCL	TEGRA_GPIO_PZ6
 #define TEGRA_GPIO_I2C5_SDA	TEGRA_GPIO_PZ7
-
-/* Laguna specific */
-
-int laguna_pinmux_init(void);
-int laguna_regulator_init(void);
-int laguna_pm358_pmon_init(void);
-int laguna_edp_init(void);
-
-/* Norrin specific */
-int norrin_regulator_init(void);
-
-/* AUO Display related GPIO */
-#define DSI_PANEL_RST_GPIO      TEGRA_GPIO_PH3 /* GMI_AD11 */
-#define LCD_RST_L               TEGRA_GPIO_PH5 /* GMI_AD13 */
-#define LCD_LR                  TEGRA_GPIO_PH6 /* GMI_AD14 */
-#define LCD_TE                  TEGRA_GPIO_PI4 /* GMI_RST_N */
-#define DSI_PANEL_BL_PWM        TEGRA_GPIO_PH1 /*GMI_AD9 */
-#define en_vdd_bl       TEGRA_GPIO_PP2 /* DAP3_DOUT */
-#define lvds_en         TEGRA_GPIO_PI0 /* GMI_WR_N */
-#define refclk_en       TEGRA_GPIO_PG4 /* GMI_AD4 */
-
-/* HID keyboard and trackpad irq same for interposer and t124 */
-#define I2C_KB_IRQ	TEGRA_GPIO_PC7
-#define I2C_TP_IRQ	TEGRA_GPIO_PW3
-
-/* TN8 specific */
-
-int tn8_regulator_init(void);
-int tn8_fixed_regulator_init(void);
-int tn8_edp_init(void);
-void tn8_new_sysedp_init(void);
-void tn8_sysedp_dynamic_capping_init(void);
-
-int tn8_p1761_pmon_init(void);
 
 /* YellowStone specific */
 int yellowstone_regulator_init(void);
